4a0fa0e00964b531537ce7f68700cdec0445c74f
Abdallah Arslan <aarslan@Abdallahs-MacBook-Pro.local>
2022-08-13 12:41:13 -0400

    Typescript updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <App />,
+    // This syntax is called a type assertion, sometimes also called a cast.
+    // This is a useful way of telling TypeScript what the real type of an
+    // expression is when you know better than the type checker.
+    document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
